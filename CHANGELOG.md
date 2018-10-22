2cb33306e8db7de0e243c6b1c024ac3fd6ba73eb
Dean Blackborough <dean@g3d-development.com>
2018-10-22 23:49:33 +0100

    New release, Costs to Expect API v1.05.0
    * New release, Costs to Expect API v1.05.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 Full changelog for the Transmute Coffee website.
 
+## v1.47.0 - New release - 2018-10-14
+
+* New release, Costs to Expect API, v1.05.0.
+
 ## v1.46.0 - New release - 2018-10-14
 
 * New release, Costs to Expect Web app, v1.02.3.
--- index.php
+++ index.php
@@ -94,8 +94,8 @@
                            class="btn btn-outline-secondary">Changelog</a>
                     </div>
                     <div class="card-footer text-muted">
-                        <small><strong>Latest release:</strong> v1.04.3<br/>
-                            <strong>Release date:</strong> 14th October 2018
+                        <small><strong>Latest release:</strong> v1.05.0<br/>
+                            <strong>Release date:</strong> 22nd October 2018
                         </small>
                     </div>
                 </div>
