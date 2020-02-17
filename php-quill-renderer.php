bc5a7f52ecc586b4febc86f611675a6c67346c57
Dean Blackborough <dean@g3d-development.com>
2020-02-17 23:07:28 +0000

    Font
    1. Switched to a new font

--- php-quill-renderer.php
+++ php-quill-renderer.php
@@ -14,6 +14,7 @@
     <title>Transmute Coffee</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
     <meta name="description" content="Open Source PHP packages by Dean Blackborough">
     <meta name="author" content="Dean Blackborough">
     <meta name="application-name" content="Transmute-coffee.com"/>
